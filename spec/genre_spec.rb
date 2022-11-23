ea07fa9ea31d05cfa10449fd86add20a6d0a725f
muskan2532150 <rg2532150@gmail.com>
2022-11-23 16:11:49 +0530

    unit Test for genre class method

---
+++ spec/genre_spec.rb
@@ -0,0 +1,10 @@
+require './modules/genre'
+
+describe Genre do
+    genre = Genre.new("aa")
+    it "Check add_item method" do
+        genre.add_item("11")
+        expect(@item.length).to eq(1)
+    end
+
+end
