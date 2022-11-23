3e395f82832e9f6d08bbd5565f61029323360f17
muskan2532150 <rg2532150@gmail.com>
2022-11-23 16:13:03 +0530

    Unit test for item class method

---
+++ spec/item_spec.rb
@@ -0,0 +1,9 @@
+require './modules/item'
+
+describe Item do
+    album = Item.new("aa","ss","cc","2012/11/22","dd")
+    it "Check can_be_archived method" do
+        value=album.send(:can_be_archived?)
+        expect(value).to eq(true)
+    end
+end
