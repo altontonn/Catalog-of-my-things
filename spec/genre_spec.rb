067d97f9dfb5b81bfe4a54316d223189eb58701a
Moathal <moathal.s.kachi@gmail.com>
2022-11-25 13:04:58 +0300

    Remove tests that are supposed in a different branch

--- spec/genre_spec.rb
+++
@@ -1,9 +0,0 @@
-require './modules/genre'
-
-describe Genre do
-  genre = Genre.new('aa')
-  it 'Check add_item method' do
-    genre.add_item('11')
-    expect(@item.length).to eq(1)
-  end
-end
--- spec/music_album_spec.rb
+++
@@ -1,9 +0,0 @@
-require './modules/music_album'
-
-describe MusicAlbum do
-  album = MusicAlbum.new('aa', 'ss', 'cc', '2012/11/22', 'dd', true)
-  it 'Check can_be_archived method' do
-    value = album.send(:can_be_archived?)
-    expect(value).to eq(true)
-  end
-end
