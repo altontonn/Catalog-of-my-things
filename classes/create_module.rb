9ca456e40a9f8031d62cc2024ace329fd65f6522
muskan2532150 <rg2532150@gmail.com>
2022-11-26 15:25:34 +0530

    Fixed rubocop

--- classes/create_module.rb
+++ classes/create_module.rb
@@ -14,13 +14,13 @@ module CreateElement
     output = input(['Genre name'])
     genre = Genre.new(output[0])
     @genres << genre
-    output = input(['Author firstname ','Author lastname'])
+    output = input(['Author firstname ', 'Author lastname'])
     author = Author.new(output[0], output[1])
     @authors << author
-    output = input(['Label title' ,'Label color'])
+    output = input(['Label title', 'Label color'])
     label = Label.new(output[0], output[1])
     @labels << label
-    output = input(['Date', 'spotify'])
+    output = input(%w[Date spotify])
     album = MusicAlbum.new(genre, author, label, output[0], output[1])
     @music_albums.push(album)
     puts 'Record is created successfully!! '
